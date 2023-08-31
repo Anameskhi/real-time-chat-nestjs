6efb96d965ced61ee156a02b6cc88827add124e1
Anameskhi <a.meskhii@gmail.com>
2023-08-31 23:53:25 +0400

    added login,registratioin,getallusers logic

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,28 +1,43 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { UserService } from '../service/user.service';
-import { Observable, of } from 'rxjs';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { UserService } from '../service/user/user.service';
+import { Observable, of, switchMap } from 'rxjs';
 import { IUser } from '../model/user.interface';
 import { CreateUserDto } from '../model/dto/create-user.dto';
+import { UserHelperService } from '../service/user-helper/user-helper.service';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { LoginUserDto } from '../model/dto/login-user.dto';
 
 @Controller('users')
 export class UserController {
     constructor(
-        private userService: UserService
+        private userService: UserService,
+        private userHelperService: UserHelperService
     ){}
 
     @Post()
-    create(@Body() createUserDto: CreateUserDto):Observable<Boolean>{
-        return of(true)
+    create(@Body() createUserDto: CreateUserDto):Observable<IUser>{
+        return this.userHelperService.CreateUserDtoTpEntity(createUserDto).pipe(
+            switchMap((user: IUser) => this.userService.create(user))
+        )
     }
 
     @Get()
-    findAll(){
-        return of("hee")
+    findAll( 
+        @Query('page') page: number = 1,
+        @Query('limit') limit: number = 10
+        ):Observable<Pagination<IUser>>{
+            limit = limit > 100? 100 : limit;
+            return this.userService.getAllUsers({page, limit, route: 'http://localhost:3000/api/users'})
+       
 
     }
 
-    @Post()
-    login(){
+    @Post('login')
+    login(@Body() loginUserDto: LoginUserDto):Observable<boolean>{
+        return this.userHelperService.loginUserDtoToEntity(loginUserDto).pipe(
+        switchMap((user: IUser) => this.userService.login(user)
+        )
+        )
 
     }
 
